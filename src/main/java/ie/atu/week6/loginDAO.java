faa157b4d9ff7e92f3a98c84e827f9d55411c6c7
matthew <g00430349@atu.ie>
2025-05-08 16:52:42 +0100

    final Git commmit

--- src/main/java/ie/atu/week6/loginDAO.java
+++ src/main/java/ie/atu/week6/loginDAO.java
@@ -35,4 +35,18 @@ public class loginDAO {
             System.out.println("Error displaying login details: " + e.getMessage());
         }
     }
+    public boolean validlogin(String username, String Password){
+        String sql = "SELECT * FROM login WHERE username = ? AND Password = ?";
+        try(Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, username);
+            stmt.setString(2, Password);
+            ResultSet rs = stmt.executeQuery();
+            return rs.next();
+        }
+        catch (SQLException e){
+            System.out.println("error login"+ e.getMessage());
+            return false;
+        }
+    }
 }
